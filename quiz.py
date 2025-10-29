f73ae9bd1ab4b914f6bcb0857d17489c51c8e204
Steven Joggi <steven.joggi@students.fhnw.ch>
2025-10-29 15:36:12 +0000

    Added Comments and Validate function

--- quiz.py
+++ quiz.py
@@ -1,10 +1,19 @@
 import json
 
+# Opening and Loading the POM.json and DIB.json files
 POM_json = open("./Data/POM.json")
 POM_data = json.load(POM_json)
 
 DIB_json = open("./Data/DIB.json")
 DIB_data = json.load(DIB_json)
 
+# "Validate" Function to check the Quiz Answers if right or wrong.
+
+
+def Validate():
+    pass
+
+
+# Closing the POM.json and DIB.json files
 POM_json.close()
 DIB_json.close()
