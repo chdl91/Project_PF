c277f987adf6bb29e7fa53baf3e510647532c6bb
Steven Joggi <steven.joggi@students.fhnw.ch>
2025-10-29 15:06:55 +0000

    Added DIB.json and tried file.close()

--- quiz.py
+++ quiz.py
@@ -1,4 +1,10 @@
 import json
 
-file_path = ("./Data/POM.json")
-open(file_path, "r")
+POM_json = ("./Data/POM.json")
+open(POM_json, "r")
+
+DIB_json = ("./Data/DIB.json")
+open(DIB_json, "r")
+
+POM_json.close()
+DIB_json.close()
