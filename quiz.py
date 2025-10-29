7940b5d54c3c1a13ae4c2e3d99a545b9b52bc688
nobrfhnw <noe.broennimann@students.fhnw.ch>
2025-10-29 15:20:06 +0000

    DIB and POM json loader build in and closed

--- quiz.py
+++ quiz.py
@@ -1,10 +1,10 @@
 import json
 
-POM_json = ("./Data/POM.json")
-open(POM_json, "r")
+POM_json = open("./Data/POM.json")
+POM_data = json.load(POM_json)
 
-DIB_json = ("./Data/DIB.json")
-open(DIB_json, "r")
+DIB_json = open("./Data/DIB.json")
+DIB_data = json.load(DIB_json)
 
 POM_json.close()
 DIB_json.close()
