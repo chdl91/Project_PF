d45c077fbed8d893af03cdfbeb359d7ea23bd8fd
Steven Joggi <steven.joggi@students.fhnw.ch>
2025-10-29 16:02:33 +0000

    Added POM_questions function and begun work on setting up range of random questions.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
 # Total Score of all the correct answers
+
+# Score begins at 0 and will be added 1 point for each correctly answered question
 score = 0
 print(score)
--- quiz.py
+++ quiz.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 # Opening and Loading the POM.json and DIB.json files
 POM_json = open("./Data/POM.json")
@@ -7,9 +8,15 @@ POM_data = json.load(POM_json)
 DIB_json = open("./Data/DIB.json")
 DIB_data = json.load(DIB_json)
 
+
+def POM_questions():
+    num_questions = 10
+    random_questions = random.sample(POM_data, num_questions)
+
+# STOPPED HERE. CONTINUE WORK HERE (29.10.2025)
+
+
 # "Validate" Function to check the Quiz Answers if right or wrong.
-
-
 def validate():
     pass
 
