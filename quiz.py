b8a3fa17c2012225c015fd9136c49c76e82729ff
nobrfhnw <noe.broennimann@students.fhnw.ch>
2025-11-12 13:21:07 +0000

    Test for commit

--- quiz.py
+++ quiz.py
@@ -23,6 +23,7 @@ def validate():
 
 # Function to determine that if the Answer is correct then we add 1 point to the score.
 # counter
+# test
 def Counter():
     if validate() == True:
         score = + 1
