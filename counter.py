405b4a49dc85b612ead911e7d02ccdc46fdde30f
nobrfhnw <noe.broennimann@students.fhnw.ch>
2025-11-12 12:57:18 +0000

    global variable  rewirte

--- counter.py
+++ counter.py
@@ -1,6 +1,8 @@
 
 # Function to determine that if the Answer is correct then we add 1 point to the score.
+global score
+
+
 def Counter():
     if validate() == True:
-        global score
         score = + 1
