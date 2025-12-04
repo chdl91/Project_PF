41801f9050d85ff24c3e21d147e16026a8fd4be6
nobrfhnw <noe.broennimann@students.fhnw.ch>
2025-12-04 15:36:44 +0000

    Deleted deadcode

--- main.py
+++ main.py
@@ -4,10 +4,3 @@ from quiz import start_menu
 
 if __name__ == "__main__":
     start_menu()
-
-
-# Total Score of all the correct answers
-
-# Score begins at 0 and will be added 1 point for each correctly answered question
-score = 0
-print(score)
